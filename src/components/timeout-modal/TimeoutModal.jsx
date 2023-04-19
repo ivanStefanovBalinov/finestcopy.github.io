33161a153dd9fd8762026a40e2089d1b03ffce35
ivanStefanovBalinov <i.balinov931@gmail.com>
2023-04-19 20:07:33 +0300

    Dev Timer

--- src/components/timeout-modal/TimeoutModal.jsx
+++ src/components/timeout-modal/TimeoutModal.jsx
@@ -3,8 +3,40 @@ import React from "react";
 
 const TimeoutModal = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="timer-container">
+      <div className="text-zone">
+        <h1>Finest Copy Agency</h1>
+        <p>
+          Сайтът е в процес на <span className="bolder">ОБНОВЯВАНЕ</span>
+        </p>
+      </div>
+      <div>
+        <section>
+          <p>10</p>
+          <p>Days</p>
+        </section>
+        <span>:</span>
+      </div>
+      <div>
+        <section>
+          <p>10</p>
+          <p>Hours</p>
+        </section>
+        <span>:</span>
+      </div>
+      <div>
+        <section>
+          <p>10</p>
+          <p>Minutes</p>
+        </section>
+        <span>:</span>
+      </div>
+      <div>
+        <section>
+          <p>10</p>
+          <p>Seconds</p>
+        </section>
+      </div>
     </div>
   );
 };
