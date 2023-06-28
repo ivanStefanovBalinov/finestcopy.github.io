e121540e134df523961c1d10fd09581b2a1a551b
ivanStefanovBalinov <i.balinov931@gmail.com>
2023-06-28 23:10:29 +0300

    setup image component

--- src/components/common/ButtonComponents/ServiceSectionButton.jsx
+++ src/components/common/ButtonComponents/ServiceSectionButton.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "./ServiceSectionButton.scss";
 
-const ServiceSectionButton = ({ path, onHover, title, buttonAlign }) => {
+const ServiceSectionButton = ({
+  path,
+  onHover,
+  title,
+  buttonAlign,
+  marginBottom,
+}) => {
   return (
     <div
-      className={"service-button-wrapper"}
-      style={{ display: "flex", justifyContent: buttonAlign }}
+      className="service-button-wrapper"
+      style={{
+        display: "flex",
+        justifyContent: buttonAlign,
+        marginBottom: marginBottom,
+      }}
     >
       <a href={path} className={onHover}>
         {title}
